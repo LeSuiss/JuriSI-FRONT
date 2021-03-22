19cd81ff96a5551845b63a65881ad2aebf8b9575
Alexis <alexis.archer44@gmail.com>
2021-03-22 16:37:15 +0100

    adding redux

--- src/App.js
+++ src/App.js
@@ -1,29 +1,9 @@
 
-import React, { createContext, useState, useRef,useEffect, useContext, useReducer } from 'react'
-import { Switch, Route, useLocation } from 'react-router-dom';
-import HomePage from './components/pages/homePage';
-import AdminPage from './components/pages/admin/AdminPage'
-
-export const dataContext = createContext(null)
+import React from "react"
 
 const App = ()=>{
 
-    const [state, dispatch] = useReducer(reducer, {} )
-    
-    function reducer (state, action){
-      
-        return !!action.payload ? {...state, ...action.payload}:{...state}
-    }
-    
-    return <dataContext.Provider value={[state, dispatch]}>
-            <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Route exact path="/admin" component={AdminPage} />
-               
-
-                
-            </Switch>
-        </dataContext.Provider>
+    return "return <pre> {JSON.stringify(, undefined, 4)} </pre>"
     
 }
 
